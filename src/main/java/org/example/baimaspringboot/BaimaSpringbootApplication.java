d33c11231cd5a74eecd659de147fce6cc8532b87
临界if <14099996+linjie999@user.noreply.gitee.com>
2025-06-27 17:54:28 +0800

    first commit

---
+++ src/main/java/org/example/baimaspringboot/BaimaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.example.baimaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaimaSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaimaSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/baimaspringboot/BaimaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.baimaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaimaSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
