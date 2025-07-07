1c15b9ae97701434c08674f652d326cd14c637fc
临界if <14099996+linjie999@user.noreply.gitee.com>
2025-07-07 14:04:31 +0800

    学到日志系统

--- src/main/java/org/example/baimaspringboot/controller/TestController.java
+++ src/main/java/org/example/baimaspringboot/controller/TestController.java
@@ -1,18 +1,42 @@
 package org.example.baimaspringboot.controller;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.example.baimaspringboot.entity.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.logging.Logger;
+
+@Slf4j
 @Controller
 public class TestController {
 
-    @ResponseBody
-    @GetMapping("/")
-    public Student index(){
-//        return "Hello World";
-        return new Student(1, "小明", "男");
 
+    @PostConstruct
+    public void init(){
+        log.info("欢迎来到卢本伟广场");
     }
+
+//    @PostConstruct
+//    public void init(){
+//        Logger logger = Logger.getLogger("test");
+//        logger.info("Hello World");
+//    }
+
+//    @Value("${test.hello}")
+//    String hello;
+
+//    @ResponseBody
+//    @GetMapping("/")
+//    public String index(){
+//        return "Hellow World";
+//    }
+//    public Student index(){
+//        return "Hello World";
+//        return new Student(1, "小明", "男");
+//
+//    }
 }
---
+++ src/main/java/org/example/baimaspringboot/runner/TestRunner.java
@@ -0,0 +1,8 @@
+package org.example.baimaspringboot.runner;
+
+public class TestRunner {
+
+
+
+
+}
